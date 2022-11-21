425288cf8b3c5419be1a4ee81d77ff5becd8fc41
sarah-ross <sarahross025@gmail.com>
2022-11-20 19:20:37 -0500

    Added functionality for city menu

--- src/world-clock.js
+++ src/world-clock.js
@@ -1,5 +1,7 @@
-function showCurrentTimes() {
-	let dateElement = document.querySelector(".date");
+function displayCurrentTimes() {
+	let dateElement = document.querySelector(
+		".new-york-date"
+	);
 	let currentDate = moment().format("MMMM Do, YYYY");
 	dateElement.innerHTML = currentDate;
 
@@ -22,4 +24,32 @@ function showCurrentTimes() {
 	tokyoTimeElement.innerHTML = tokyoTime;
 }
 
-setInterval(showCurrentTimes, 1000);
+setInterval(displayCurrentTimes, 1000);
+
+function updateCityData(event) {
+	let cityTimezone = event.target.value;
+	console.log(cityTimezone);
+	let selectedCity = cityTimezone
+		.replace("_", " ")
+		.split("/")[1];
+	let cityTime = moment().tz(cityTimezone);
+	let cityDataElement =
+		document.querySelector("#display-city");
+	cityDataElement.innerHTML = `
+				<div class="city-and-time">
+					<h3>${selectedCity}</h3>
+					<h2 id="new-york-time">${cityTime.format(
+						"hh:mm:ss[<small>]A[</small]"
+					)}
+					)}</h2>
+				</div>
+				<div class="date">${cityTime.format(
+					"MMMM Do, YYYY"
+				)}</div>`;
+}
+
+let citiesSelectElement = document.querySelector("#cities");
+citiesSelectElement.addEventListener(
+	"change",
+	updateCityData
+);
