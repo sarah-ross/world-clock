81893f2d223c618993b8464571a3a75f726c6022
sarah-ross <sarahross025@gmail.com>
2022-11-20 21:53:52 -0500

    Improved responsive styling, added local time functionality

--- src/world-clock.js
+++ src/world-clock.js
@@ -1,10 +1,10 @@
 function displayCurrentTimes() {
+	// New York
 	let dateElement = document.querySelector(
 		".new-york-date"
 	);
 	let currentDate = moment().format("MMMM Do, YYYY");
 	dateElement.innerHTML = currentDate;
-
 	let newYorkTimeElement = document.querySelector(
 		"#new-york-time"
 	);
@@ -12,7 +12,7 @@ function displayCurrentTimes() {
 		.tz("America/New_York")
 		.format("hh:mm:ss[<small>]A[</small>]");
 	newYorkTimeElement.innerHTML = newYorkTime;
-
+	// Tokyo
 	document.querySelector(".tokyo-date").innerHTML = moment()
 		.tz("Asia/Tokyo")
 		.format("MMMM Do, YYYY");
@@ -22,13 +22,24 @@ function displayCurrentTimes() {
 		.tz("Asia/Tokyo")
 		.format("hh:mm:ss[<small>]A[</small>]");
 	tokyoTimeElement.innerHTML = tokyoTime;
+	let sydneyTimeElement =
+		document.querySelector("#sydney-time");
+
+	document.querySelector(".sydney-date").innerHTML =
+		moment().tz("Australia/Sydney").format("MMMM Do, YYYY");
+	let sydneyTime = moment()
+		.tz("Australia/Sydney")
+		.format("hh:mm:ss[<small>]A[</small>]");
+	sydneyTimeElement.innerHTML = sydneyTime;
 }
 
 setInterval(displayCurrentTimes, 1000);
 
 function updateCityData(event) {
 	let cityTimezone = event.target.value;
-	console.log(cityTimezone);
+	if (cityTimezone === "current") {
+		cityTimezone = moment.tz.guess();
+	}
 	let selectedCity = cityTimezone
 		.replace("_", " ")
 		.split("/")[1];
